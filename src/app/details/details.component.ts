35a081d0999cd4c6cd1ed13c62fed3ccdec12a15
BoomBakar <abubakardar608@gmail.com>
2023-08-23 11:58:04 +0500

    Services added

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,6 +2,8 @@ import { Component,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import {ActivatedRoute} from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
@@ -10,16 +12,26 @@ import {ActivatedRoute} from '@angular/router';
   template: `
     <section class="details">
       <h1>Details</h1>
-      <p class="details-city">City: Houston</p>
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{housingLocation.name}}</h2>
+      <p class="listing-city">City: {{housingLocation.city}}</p>
+      <p class="listing-state">State: {{housingLocation.state}}</p>
+      <p class="listing-price">Price: \${{housingLocation.price}} / month</p>
+      <p class="listing-units">Units: {{housingLocation.availableUnits}}</p>
+      <p class="listing-wifi">Wifi: {{housingLocation.wifi}}</p>
+      <p class="listing-laundry">Laundry: {{housingLocation.laundry}}</p>
+
     </section>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId: number;
+  housingService: HousingService = inject(HousingService);
+  housingLocation!: HousingLocation;
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params["id"]);
-   }
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
 
+   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -22,118 +23,9 @@ import { HousingLocation } from '../housing-location';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 0,
-      name: 'The DJ House',
-      city: 'Houston',
-      state: 'LA',
-      price: 1000,
-      photo: 'assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 1,
-      name: 'The Bay Club',
-      city: 'Baytown',
-      state: 'TX',
-      price: 1000,
-      photo: 'assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 2,
-      name: 'The Dayton',
-      city: 'Dayton',
-      state: 'TX',
-      price: 1200,
-      photo: 'assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 3,
-      name: 'The Woodlands',
-      city: 'The Woodlands',
-      state: 'RX',
-      price: 1500,
-      photo: 'assets/r-architecture-GGupkreKwxA-unsplash.jpg',
-      availableUnits: 1,
-      wifi: false,
-      laundry: true
-    },
-    {
-      
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      price: 800,
-      photo: '/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      price: 1200,
-      photo: '/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      price: 1000,
-      photo: '/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/r-architecture-GGupkreKwxA-unsplash.jpg',
-      availableUnits: 2,
-      price: 1200,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      price: 1000,
-      photo: '/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      price: 1000,
-      photo: '/assets/webaliser-_TPTXZd9mOo-unsplash.jpg',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true
-    }
-
-  ];
+  housingLocationList: HousingLocation[] = [ ];
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.getHousingLocations();
+  }
 }
