669e379247eeb737fd7993d5fbc64672d9a09a2a
BoomBakar <abubakardar608@gmail.com>
2023-08-23 10:26:21 +0500

    Basic angular app made

---
+++ src/app/housing-location.ts
@@ -0,0 +1,11 @@
+export interface HousingLocation {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    price: number;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+}
